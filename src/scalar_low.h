e9f3067e3e5c1a5208c26c019504900951953bb3
Andrew Poelstra <apoelstra@wpsoftware.net>
2018-03-13 15:24:08 +0000

    f hack: add dummy scalar_const function so that the exhaustive tests will compile

--- src/scalar_low.h
+++ src/scalar_low.h
@@ -9,6 +9,8 @@
 
 #include <stdint.h>
 
+#define SECP256K1_SCALAR_CONST(d7, d6, d5, d4, d3, d2, d1, d0) 0xDEADBEEF
+
 /** A scalar modulo the group order of the secp256k1 curve. */
 typedef uint32_t secp256k1_scalar;
 
